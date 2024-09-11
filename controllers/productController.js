2def9970f2ccc8b54e655991d56ec79d73abe0b0
unknown <n200119@rguktn.ac.in>
2024-09-11 23:25:16 +0530

    modified cd to cb

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
     cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    cd(null, Date.now() + path.extname(file.originalname));
+    cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
